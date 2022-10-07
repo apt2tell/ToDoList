215c521ff73332b5f91d4b68d6ace2aa84708f40
Okechukwu <okei.muokwugwo@outlook.com>
2022-10-07 09:26:44 +0100

    Add test for addList method of EventObj class

---
+++ src/modules/index.test.js
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import EventObj from './addEvent.js';
+
+const event = new EventObj();
+event.addList = jest.fn();
+event.delEvent = jest.fn();
+describe('implementation of addList and delList testing', () => {
+  test('Add one new item to the list', () => {
+    document.body.innerHTML = '<div>'
+        + '  <ul id="list"><li></li></ul>'
+        + '</div>';
+    jest.fn();
+    const list = document.querySelectorAll('#list li');
+    expect(list).toHaveLength(1);
+  });
+});
