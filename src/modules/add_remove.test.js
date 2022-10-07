812c1b77140c53cc4a38e73f9d1bc113b0ecd9b9
= <adedejifavour6@gmail.com>
2022-10-07 18:32:44 +0100

    Import the files path properly

--- src/modules/add_remove.test.js
+++ src/modules/add_remove.test.js
@@ -1,13 +1,14 @@
-import addTask from '../modules/add-task.js';
-import storedArray from '../modules/storage.js';
-import removeTask from '../modules/remove-task.js';
-​
+import addTask from './add-task.js';
+import storedArray from './storage.js';
+import removeTask from './remove-task.js';
+
 describe('test addTask and removeTask', () => {
   // Testing add task function
   test('test add task function', () => {
     addTask('first task');
     addTask('second task');
     addTask('third task');
+    expect(storedArray[0].description).toBe('first task');
     expect(storedArray[1].description).toBe('second task');
     expect(storedArray.length).toEqual(3);
   });
